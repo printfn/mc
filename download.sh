2e6eaeedacaba354c51bd34e06fd05b34d3bcb1e
printfn <printfn@users.noreply.github.com>
2022-09-15 13:07:28 +1200

    Update download.sh

--- download.sh
+++ download.sh
@@ -61,81 +61,86 @@ if [[ "$foundcmd" == false ]]; then
     exit 1
 fi
 
-if [[ "$verbose" == true ]]; then
-    echo "Downloading version manifest from https://launchermeta.mojang.com/mc/game/version_manifest.json..."
-fi
-data=$(curl -sS $curlflags https://launchermeta.mojang.com/mc/game/version_manifest.json)
+download_mc() {
+    local mc_command="$1"
+    if [[ "$verbose" == true ]]; then
+        echo "Downloading version manifest from https://launchermeta.mojang.com/mc/game/version_manifest.json..."
+    fi
+    data=$(curl -sS $curlflags https://launchermeta.mojang.com/mc/game/version_manifest.json)
 
-if [[ "$command" == "latest" ]]; then
-    version=$(echo "$data" | jq -r .latest.release)
-elif [[ "$command" == "list-latest" ]]; then
-    echo "$data" | jq -r ".latest.release"
-    exit
-elif [[ "$command" == "latest-snapshot" ]]; then
-    version=$(echo "$data" | jq -r .latest.snapshot)
-elif [[ "$command" == "list-latest-snapshot" ]]; then
-    echo "$data" | jq -r ".latest.snapshot"
-    exit
-elif [[ "$command" == "list" ]]; then
-    echo "$data" | jq -r ".versions[].id"
-    exit
-else
-    version="$command"
-fi
-
-if [[ "$verbose" == true ]]; then
-    echo "Found version $version"
-elif [[ "$quiet" != true ]]; then
-    echo "Downloading version $version..."
-fi
-
-url=$(echo "$data" | jq -r ".versions[] | select(.id == \"$version\") | .url")
-if [[ -z "$url" ]]; then
-    echo "error: unknown version '$version'" >&2
-    exit 1
-fi
-
-if [[ "$verbose" == true ]]; then
-    echo "Downloading version info from $url..."
-fi
-
-data=$(curl -Ss $curlflags "$url" | jq .downloads.server)
-url=$(echo "$data" | jq -r .url)
-
-if [[ "$verbose" == true ]]; then
-    echo "Downloading server.jar from $url..."
-fi
-
-sha1=$(echo "$data" | jq -r .sha1)
-
-curlsilent=""
-if [[ "$quiet" == true ]]; then
-    curlsilent="--silent"
-fi
-
-curl $curlsilent $curlflags -# -o server.jar "$url"
-
-if command -v sha1sum &>/dev/null; then
-    if sha1sum --check --strict --status <(echo "$sha1 server.jar"); then
-        if [[ "$verbose" == true ]]; then
-            echo "Successfully verified checksum with \`sha1sum\`"
-        fi
+    if [[ "$mc_command" == "latest" ]]; then
+        version=$(echo "$data" | jq -r .latest.release)
+    elif [[ "$mc_command" == "list-latest" ]]; then
+        echo "$data" | jq -r ".latest.release"
+        exit
+    elif [[ "$mc_command" == "latest-snapshot" ]]; then
+        version=$(echo "$data" | jq -r .latest.snapshot)
+    elif [[ "$mc_command" == "list-latest-snapshot" ]]; then
+        echo "$data" | jq -r ".latest.snapshot"
+        exit
+    elif [[ "$mc_command" == "list" ]]; then
+        echo "$data" | jq -r ".versions[].id"
+        exit
     else
-        echo "error: checksum mismatch" >&2
+        version="$mc_command"
+    fi
+
+    if [[ "$verbose" == true ]]; then
+        echo "Found version $version"
+    elif [[ "$quiet" != true ]]; then
+        echo "Downloading version $version..."
+    fi
+
+    url=$(echo "$data" | jq -r ".versions[] | select(.id == \"$version\") | .url")
+    if [[ -z "$url" ]]; then
+        echo "error: unknown version '$version'" >&2
         exit 1
     fi
-elif command -v shasum &>/dev/null; then
-    # two spaces are necessary, otherwise `shasum` returns an error`
-    if shasum --algorithm 1 --check --strict --status <(echo "$sha1  server.jar"); then
-        if [[ "$verbose" == true ]]; then
-            echo "Successfully verified checksum with \`shasum\`"
+
+    if [[ "$verbose" == true ]]; then
+        echo "Downloading version info from $url..."
+    fi
+
+    data=$(curl -Ss $curlflags "$url" | jq .downloads.server)
+    url=$(echo "$data" | jq -r .url)
+
+    if [[ "$verbose" == true ]]; then
+        echo "Downloading server.jar from $url..."
+    fi
+
+    sha1=$(echo "$data" | jq -r .sha1)
+
+    curlsilent=""
+    if [[ "$quiet" == true ]]; then
+        curlsilent="--silent"
+    fi
+
+    curl $curlsilent $curlflags -# -o server.jar "$url"
+
+    if command -v sha1sum &>/dev/null; then
+        if sha1sum --check --strict --status <(echo "$sha1 server.jar"); then
+            if [[ "$verbose" == true ]]; then
+                echo "Successfully verified checksum with \`sha1sum\`"
+            fi
+        else
+            echo "error: checksum mismatch" >&2
+            exit 1
+        fi
+    elif command -v shasum &>/dev/null; then
+        # two spaces are necessary, otherwise `shasum` returns an error`
+        if shasum --algorithm 1 --check --strict --status <(echo "$sha1  server.jar"); then
+            if [[ "$verbose" == true ]]; then
+                echo "Successfully verified checksum with \`shasum\`"
+            fi
+        else
+            echo "error: checksum mismatch" >&2
+            exit 1
         fi
     else
-        echo "error: checksum mismatch" >&2
-        exit 1
+        # skip verification
+        echo "warning: neither \`sha1sum\` or \`shasum\` is installed: skipping hash verification" >&2
+        exit
     fi
-else
-    # skip verification
-    echo "warning: neither \`sha1sum\` or \`shasum\` is installed: skipping hash verification" >&2
-    exit
-fi
+}
+
+download_mc "$command"
