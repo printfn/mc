3bdbb04247b33c3d58ad6e04bb475756bc6db739
printfn <printfn@users.noreply.github.com>
2022-09-15 14:10:57 +1200

    Support downloading forge installers

--- download.sh
+++ download.sh
@@ -16,6 +16,9 @@ Downloads a specified Minecraft server.jar file
 <version> can be a version number like '1.18.2', or it can be 'latest',
     'latest-snapshot', 'list', 'list-latest' or 'list-latest-snapshot'
 
+Forge installers can also be downloaded by specifying 'forge:1.18.2',
+    'forge:1.18.2-40.1.80' or 'forge:list'
+
 Flags:
     --curl-flag <flag>  passes the specified flag through to \`curl\`
 -h  --help              show this help screen
@@ -61,12 +64,47 @@ if [[ "$foundcmd" == false ]]; then
     exit 1
 fi
 
+mycurl() {
+    # shellcheck disable=SC2086
+    curl -f $curlflags "$@"
+}
+
+verify_hash() {
+    local filename="$1"
+    local sha1="$2"
+
+    if command -v sha1sum &>/dev/null; then
+        if sha1sum --check --strict --status <(echo "$sha1 $filename"); then
+            if [[ "$verbose" == true ]]; then
+                echo "Successfully verified checksum with \`sha1sum\`"
+            fi
+        else
+            echo "error: checksum mismatch" >&2
+            exit 1
+        fi
+    elif command -v shasum &>/dev/null; then
+        # two spaces are necessary, otherwise `shasum` returns an error`
+        if shasum --algorithm 1 --check --strict --status <(echo "$sha1  $filename"); then
+            if [[ "$verbose" == true ]]; then
+                echo "Successfully verified checksum with \`shasum\`"
+            fi
+        else
+            echo "error: checksum mismatch" >&2
+            exit 1
+        fi
+    else
+        # skip verification
+        echo "warning: neither \`sha1sum\` or \`shasum\` is installed: skipping hash verification" >&2
+        exit
+    fi
+}
+
 download_mc() {
     local mc_command="$1"
     if [[ "$verbose" == true ]]; then
         echo "Downloading version manifest from https://launchermeta.mojang.com/mc/game/version_manifest.json..."
     fi
-    data=$(curl -sS $curlflags https://launchermeta.mojang.com/mc/game/version_manifest.json)
+    data=$(mycurl -sS https://launchermeta.mojang.com/mc/game/version_manifest.json)
 
     if [[ "$mc_command" == "latest" ]]; then
         version=$(echo "$data" | jq -r .latest.release)
@@ -101,7 +139,7 @@ download_mc() {
         echo "Downloading version info from $url..."
     fi
 
-    data=$(curl -Ss $curlflags "$url" | jq .downloads.server)
+    data=$(mycurl -sS "$url" | jq .downloads.server)
     url=$(echo "$data" | jq -r .url)
 
     if [[ "$verbose" == true ]]; then
@@ -115,32 +153,72 @@ download_mc() {
         curlsilent="--silent"
     fi
 
-    curl $curlsilent $curlflags -# -o server.jar "$url"
+    mycurl $curlsilent -# -o server.jar "$url"
 
-    if command -v sha1sum &>/dev/null; then
-        if sha1sum --check --strict --status <(echo "$sha1 server.jar"); then
-            if [[ "$verbose" == true ]]; then
-                echo "Successfully verified checksum with \`sha1sum\`"
-            fi
-        else
-            echo "error: checksum mismatch" >&2
-            exit 1
+    verify_hash server.jar "$sha1"
+}
+
+download_forge_version() {
+    # downloads a specific forge version, e.g. '1.18.2-40.1.80'
+    local longversion="$1"
+
+    meta=$(mycurl -sS "https://files.minecraftforge.net/net/minecraftforge/forge/$longversion/meta.json")
+    installer_md5=$(echo "$meta" | jq -r ".classifiers.installer.jar")
+    if [[ "$verbose" == true ]]; then
+        echo "MD5 checksum is $installer_md5"
+    fi
+
+    local filename="forge-$longversion-installer.jar"
+    mycurl -#O "https://maven.minecraftforge.net/net/minecraftforge/forge/$longversion/$filename"
+}
+
+download_forge() {
+    local forge_version="$1"
+
+    local url="https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"
+    if [[ "$verbose" == true ]]; then
+        echo "Downloading forge promotions from $url..."
+    fi
+    local promotions
+    promotions=$(mycurl -sS "$url")
+
+    if [[ "$forge_version" == "list" ]]; then
+        mycurl -sS "https://files.minecraftforge.net/net/minecraftforge/forge/maven-metadata.json" | jq -r "flatten | .[]"
+        echo "$promotions" | jq -r ".promos | keys[]"
+        return
+    fi
+    
+    local target_version
+
+    target_version=$(echo "$promotions" | jq -r ".promos[\"$forge_version\"]")
+    if [[ -n "$target_version" && "$target_version" != "null" ]]; then
+        local mc_version
+        mc_version=$(echo "$forge_version" | grep -o "[0-9.]\\+")
+        if [[ "$verbose" == true ]]; then
+            echo "Found matching forge release '$mc_version-$target_version'"
         fi
-    elif command -v shasum &>/dev/null; then
-        # two spaces are necessary, otherwise `shasum` returns an error`
-        if shasum --algorithm 1 --check --strict --status <(echo "$sha1  server.jar"); then
-            if [[ "$verbose" == true ]]; then
-                echo "Successfully verified checksum with \`shasum\`"
-            fi
-        else
-            echo "error: checksum mismatch" >&2
-            exit 1
+        download_forge_version "$mc_version-$target_version"
+        return
+    fi
+    target_version=$(echo "$promotions" | jq -r ".promos[\"${forge_version}-latest\"]")
+    if [[ -n "$target_version" && "$target_version" != "null" ]]; then
+        if [[ "$verbose" == true ]]; then
+            echo "Found matching forge release '$forge_version-$target_version'"
         fi
+        download_forge_version "$forge_version-$target_version"
+        return
+    fi
+    download_forge_version "$forge_version"
+}
+
+download() {
+    local name="$1"
+    if [[ "$name" =~ ^forge: ]]; then
+        # shellcheck disable=SC2001
+        download_forge "$(echo "$name" | sed "s/^forge://")"
     else
-        # skip verification
-        echo "warning: neither \`sha1sum\` or \`shasum\` is installed: skipping hash verification" >&2
-        exit
+        download_mc "$name"
     fi
 }
 
-download_mc "$command"
+download "$command"
